76281475d33fb5cda229f615027531b6b0789e61
Polyhedral Structures Laboratory <64336225+PolyhedralStructuresLaboratory@users.noreply.github.com>
2023-01-27 11:00:41 -0500

    Update sceneSetupEp3.js

--- js/sceneSetupEp3.js
+++ js/sceneSetupEp3.js
@@ -1,7 +1,7 @@
 import '/style.css'; //setup basic visual factors for the overall web
 
 import * as THREE from "../node_modules/three/build/three.module.js";
-import * as Geo from 'js/functions.js';
+import * as Geo from '/js/functions.js';
 import { createMultiMaterialObject } from '../node_modules/three/examples/jsm/utils/SceneUtils';
 
 
